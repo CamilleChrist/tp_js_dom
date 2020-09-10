8b2c12f3ed7a84ba4b9bd4e23570e886ac9cc972
Camille <christ.camille@orange.fr>
2020-09-10 10:59:34 +0200

    1

--- src/dom.js
+++ src/dom.js
@@ -1,26 +1,31 @@
 // Changez la couleur de fond du body en #992323
 // Ne modifiez pas le code HTML de la page directement
 // votre code
+document.body.style.backgroundColor = "#992323";
 
 // Changez la couleur du texte du body en #fff
 // votre code
+document.body.style.color = "#fff";
 
 // Supprimez la ligne suivante
-var workspace = null;
+// var workspace = null;
 // Récupérez l'élément div "workspace" par son id
 // Enregistrez-le dans une variable workspace
 // votre code
+var workspace = document.getElementById("workspace");
 
 // Affichez dans la console le nombre d'éléments enfants que contient
 // l'élément workspace
 // votre code
+console.log(workspace.childElementCount);
 
 // Récupérez le premier paragraphe contenu dans workspace
 // vous ne devez pas ajouter d'id
 // Supprimez la ligne suivante
-var p = null;
+// var p = null;
 // Affectez le résultat à la variable p
 // votre code
+var p = workspace.childNodes[1].textContent;
 console.log(p);
 
 // Ajoutez le code suivant : "<h3>Hello JS world!</h3>"
@@ -28,17 +33,34 @@ console.log(p);
 // Cet élément doit être ajouté avant le premier paragraphe
 // contenu dans workspace
 // votre code
+var titre3 = document.createElement("h3");
+titre3.innerHTML = "Hello JS world!";
+workspace.insertBefore(titre3, workspace.firstChild);
+
 
 const initTime = 10;
 
 // Complétez la fonction runChrono pour qu'elle affiche un décompte
 // de n à 0, dans une div (id: counter), placée sous la balise h3 précédemment ajoutée
-// Vous ne devez pas toucher au code HTML, et le h3 doit continuer à s'afficher
-function runChrono(/* votre code */) {
-  // votre code
+// Vous ne devez pas toucher au code HTML, et le h3 doit continuer à s'
+
+function runChrono(valeur) {
+    const divCounter = document.createElement('div');
+    divCounter.setAttribute('id', 'counter');
+    divCounter.innerHTML = valeur;
+    workspace.insertBefore(divCounter, workspace.firstChild.nextSibling.nextSibling);
+
+    intervale = setInterval(() => {
+        valeur--;
+        divCounter.innerHTML = valeur;
+
+        if (valeur === 0) {
+            clearInterval(intervale);
+        }
+    }, 1000);
 }
 
-console.log(runChrono(/* votre code */));
+// console.log(runChrono(initTime));
 
 // Ajoutez à l'élément workspace un bouton permettant de déclencher
 // le décompte lors d'un clic, avec le texte "Start countdown"
@@ -47,3 +69,17 @@ console.log(runChrono(/* votre code */));
 // Le décompte est réinitialisé lorsque l'utilisateur clique une
 // nouvelle fois sur le bouton
 // votre code
+var bouton = document.createElement("button");
+bouton.innerHTML = "Start countdown";
+workspace.insertBefore(bouton, workspace.firstChild.nextSibling);
+bouton.addEventListener("click", function () {
+    // runChrono(initTime);
+    bouton.classList.toggle("active");
+    if (bouton.classList.contains("active")) {
+        runChrono(initTime);
+    } else {
+        clearInterval(intervale);
+        document.querySelector("#counter:last-of-type").remove();
+    }
+
+});
--- src/simple.js
+++ src/simple.js
@@ -2,75 +2,99 @@
 // Ajoutez dans un second temps une propriété de type booléen
 // Retournez l'objet
 function createObject() {
-	// votre code
+    // votre code
+    // Syntaxe avec new Object()
+    const objet = new Object();
+    objet.nom = "carton";
+    return objet;
 }
 
-console.log(createObject());
+console.log("Exo 1 : " + createObject());
 
 // Retournez un objet avec trois propriétés :
 // - val1: la valeur du paramètre v1
 // - absVal2: la valeur absolue du paramètre v2
 // - somme de v1 et v2
 function additionObject(v1, v2) {
-	// votre code
+    // votre code
+    const code = new Object();
+    code.val1 = v1;
+    code.absVal2 = Math.abs(v2);
+    code.somme = v1 + v2;
+
+    return code;
 }
 
-console.log(additionObject(2, 6));
-console.log(additionObject(-5, -10));
+console.log(additionObject("Exo 2 : " + 2, 6));
+console.log(additionObject("Exo 2 : " +-5, -10));
 
 // Retournez un tableau avec uniquement des nombres impairs supérieurs à 0
 // Si le tab passé en paramètre est null, retournez un tableau vide
 function removeEvenNumbers(tab) {
-	// votre code
+    // votre code
+	if (tab == null) {
+		tableau = []
+	} else {
+
+	}
 }
 
-console.log(removeEvenNumbers([]));
-console.log(removeEvenNumbers(null));
-console.log(removeEvenNumbers([-1, 1, 2, 0, 3, 4, 12, 11]));
+console.log(removeEvenNumbers("Exo 3 : " +[]));
+console.log(removeEvenNumbers("Exo 3 : " +null));
+console.log(removeEvenNumbers("Exo 3 : " +[-1, 1, 2, 0, 3, 4, 12, 11]));
 
 // Complétez la fonction testNumNeg qui retourne vrai si au
 // moins un élément du tableau en entrée est un nombre et
 // qu'il a une valeur négative
 function testNumNeg(tab) {
-	// votre code
+    // votre code
 }
 
-console.log(testNumNeg([1, "kiwi", true, -2]));
-console.log(testNumNeg([0, "orange", false]));
-console.log(testNumNeg([0, 4, 8]));
+console.log(testNumNeg("Exo 4 : " +[1, "kiwi", true, -2]));
+console.log(testNumNeg("Exo 4 : " +[0, "orange", false]));
+console.log(testNumNeg("Exo 4 : " +[0, 4, 8]));
 
 // Complétez la fonction ci-dessous pour retourner un
 // tableau contenant la table de multiplication (jusqu'à 10 inclus)
 // du nombre passé en paramètre, par exemple :
 // n = 5 : [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
 function multTable(n) {
-	// votre code
+    // votre code
+	let table = [];
+	for (var i = 0; i < 11; i++) {
+		table[i]=n*i;
+	}
+	return table;
 }
 
-console.log("Table de multiplication de 1", multTable(1));
-console.log("Table de multiplication de 5", multTable(5));
+console.log("Exo 5 - Table de multiplication de 1", multTable(1));
+console.log("Exo 5 - Table de multiplication de 5", multTable(5));
 
 // En utilisant une boucle et la fonction précédente,
 // retournez un tableau contenant toutes les tables de multiplication
 // (jusqu'à 10 inclus) pour les nombres allant de 0 à la valeur d'un
 // paramètre max (inclus)
 function multTables(max) {
-	// votre code
+	let tables = [];
+	for (var j = 0; j < max+1; j++) {
+		tables[j]=multTable(j);
+	}
+	return tables;
 }
 
-console.log(multTables(5));
+console.log("Ex 6 : " + multTables(5));
 
 const products = [
-	{
-		model: "Xiaomi Mi 9",
-		size: "74.7 mm X 157.5 mm X 7.6 mm",
-		weight: "173"
-	},
-	{
-		model: "Samsung Galaxy A21",
-		size: "75.3 mm X 163.7 mm X 9.0 mm",
-		weight: "192"
-	}
+    {
+        model: "Xiaomi Mi 9",
+        size: "74.7 mm X 157.5 mm X 7.6 mm",
+        weight: "173"
+    },
+    {
+        model: "Samsung Galaxy A21",
+        size: "75.3 mm X 163.7 mm X 9.0 mm",
+        weight: "192"
+    }
 ];
 
 // Modifiez la fonction pour qu'elle compte le nombre de caractères
@@ -81,7 +105,9 @@ const products = [
 // Vous ne devez pas créer d'objet à l'intérieur de la fonction,
 // ni utiliser de variable globale
 function howLongIsIt(obj) {
-	// votre code
+    // votre code
+    var objet = obj.replace(/ /g, "").length;
+
 }
 
 // Vous devriez avoir à modifier cette ligne...
@@ -106,13 +132,10 @@ console.log(newProduct0);
 // Complétez votre code avec une fonction transformant tous les objets
 // compris dans le tableau passé en paramètre de la même manière que précédemment
 function howLongIsItTab(tab) {
-	// votre code
+    // votre code
 }
 
 // Vous devriez avoir à modifier cette ligne...
 let newProducts = howLongIsItTab(products);
 console.log(products);
 console.log(newProducts);
-
-
-
